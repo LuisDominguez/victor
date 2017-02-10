6a9a7a6aae5a3a745937290cca52c8a945391fd3
VictorIbarra <victor_orozco356@hotmail.com>
2017-02-10 13:39:12 -0600

    avance  del proyecto blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208194100) do
+ActiveRecord::Schema.define(version: 20170210172452) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nombre"
     t.string   "title"
-    t.text     "body"
+    t.text     "body",         limit: 65535
     t.integer  "visits_count"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "articules", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nombre"
+    t.text     "empresa",      limit: 65535
+    t.text     "comentario",   limit: 65535
+    t.text     "link_youtube", limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
