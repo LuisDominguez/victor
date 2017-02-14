b749f51b817eb7aa9d51d7340a2f9481b4909c72
VictorIbarra <victor_orozco356@hotmail.com>
2017-02-14 13:37:16 -0600

    nuevos cambios   (revise )

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
   #get /articles
   def index
+
+  # todos los registros
     @articles =  Article.all
   end
 
@@ -15,9 +17,14 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   #post /articles
   def create
-    @article = Article.new(title: params[:article][:title],body: params[:article][:body])
+    @article = Article.new(article_params)
+
 
     if @article.save
       redirect_to @article
@@ -26,8 +33,28 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy  #destroy eliminar el objeto de la bd
+    redirect_to articles_path
+  end
+
   #put /articles/: id
   def update
-
+    # update
+    # @article.update_attributes({title: 'nuevo titulo'})
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit 
+    end
   end
+
+  private
+
+  def article_params
+      params.require(:article).permit(:title,:body)
+  end
+
 end
